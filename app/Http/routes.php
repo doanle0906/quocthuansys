4b3848ad63e336878fc1b49fe5c8b183510dbaf0
DoanLe96 <31028561+doanle0906@users.noreply.github.com>
2018-02-25 16:12:00 +0700

    Add layout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Home - @yield('title')</title>
+    <link rel="shortcut icon" href="{{asset('public/favicon/faviconbb80.png')}}" type="image/png" />
+    <meta charset="utf-8" />
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' /><![endif]-->
+    <title>
+        The Coffee House - Delivery 1800 6936
+    </title>
+
+    <meta name="description" content="Hơn 70 cửa hàng tại Thành Phố Hồ Chí Minh, Hà Nội, Đà Nẵng và Biên Hòa sẵn sàng phục vụ bạn. Giao hàng miễn phí tại các thành phố qua chỉ cần gọi 1800 6936 !" />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=0' name='viewport' />
+    <link rel="canonical" href="index.html" />
+
+    <script src={{asset('public/design/plugins/jquery/jquery-1.11.3.min.js')}}></script>
+    <script src={{asset('public/design/plugins/bootstrap/3.3.6/js/bootstrap.min.js')}}></script>
+    <script src={{asset('public/design/js/option_selection.min.js')}}></script>
+    <script src={{asset('public/design/js/api.jquery.min.js')}}></script>
+
+
+    <script src={{asset('public/js/modernizr.minbb80.js')}} type='text/javascript'></script>
+    <script src={{asset('public/design/theme-default/html5shiv.js')}}></script>
+    <script src={{asset('public/design/theme-default/jquery-migrate-1.2.0.min.js')}}></script>
+    <script src={{asset('public/design/theme-default/jquery.touchSwipe.min.js')}} type='text/javascript'></script>
+    <script src={{asset('public/design/js/haravan.plugin.1.0.min.js')}}></script>
+    <script src={{asset('public/design/js/owl.carousel.js')}} type='text/javascript'></script>
+    <script src={{asset('public/js/jquery.selectric.minbb80.js')}} type='text/javascript'></script>
+    <script src={{asset('public/js/jquery.bxslider.minbb80.js')}} type='text/javascript'></script>
+
+    <script src={{asset('public/js/jquery.scrollifybb80.js')}} type='text/javascript'></script>
+
+    <script src={{asset('public/js/jQuery.scrollSpeedbb80.js')}} type='text/javascript'></script>
+    <script src={{asset('public/js/parallaxbb80.js?')}} type='text/javascript'></script>
+    <script src={{asset('public/js/jquery.flexsliderbb80.js')}} type='text/javascript'></script>
+    <script src={{asset('public/js/scriptsbb80.js')}} type='text/javascript'></script>
+    <script src={{asset('public/js/wowbb80.js')}} type='text/javascript'></script>
+
+    <!--------------CSS----------->
+    <link rel="stylesheet" href={{asset('public/design/plugins/bootstrap/3.3.6/css/bootstrap.min.css')}}>
+    <link rel="stylesheet" href={{asset('public/design/plugins/font-awesome/4.5.0/css/font-awesome.min.css')}}>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,600,300' rel='stylesheet' type='text/css'>
+    <link href={{asset('public/design/css/owl.carousel.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/design/css/owl.theme.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/owl.transitionsbb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/bxsliderbb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/flexsliderbb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/selectricbb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/stylesbb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/adaptivebb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+    <link href={{asset('public/css/animatebb80.css')}} rel='stylesheet' type='text/css'  media='all'  />
+
+</head>
+<body class="home ">
+    @yield('content')
+</body>
+</html>
